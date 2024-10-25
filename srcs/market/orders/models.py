24bdd30772ba8de44bbdd6302a59fdd1ee459903
jhonata <jhon@DESKTOP-MAPFS0I.>
2024-10-25 15:46:13 +0100

    feat: dashboard in progress. html added

--- srcs/market/orders/models.py
+++ srcs/market/orders/models.py
@@ -8,4 +8,4 @@ class Order(models.Model):
     order_date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"Order {self.order_id} - {self.user.name} comprou {self.quantity} {self.product.name}(s)"
+        return f'Order {self.order_id} by {self.user}'
