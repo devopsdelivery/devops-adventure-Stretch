cf857cfe2e834958aaee0fa1bb34ac4ad5b8ef8a
jhonata <jhon@DESKTOP-MAPFS0I.>
2024-10-25 15:42:20 +0100

    feat: dashboard in progress. html added

--- srcs/market/orders/models.py
+++ srcs/market/orders/models.py
@@ -1,8 +1,11 @@
 from django.db import models
 
 class Order(models.Model):
-	order_id = models.AutoField(primary_key=True)
-	user_id = models.IntegerField(foreign_key=True)
-	product_id = models.IntegerField(foreign_key=True)
-	quantity = models.IntegerField()
-	order_date = models.DateTimeField(auto_now_add=True)
+    order_id = models.AutoField(primary_key=True)
+    user = models.ForeignKey('users.User', on_delete=models.CASCADE)
+    product = models.ForeignKey('products.Product', on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+    order_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'Order {self.order_id} by {self.user}'
