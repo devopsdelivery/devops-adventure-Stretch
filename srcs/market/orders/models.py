4fef8e941f6ef9beddfc38d14e61cc33a6f37d4e
jhonata <jhon@DESKTOP-MAPFS0I.>
2024-10-25 14:33:51 +0100

    feat: order.models

--- srcs/market/orders/models.py
+++ srcs/market/orders/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Order(models.Model):
+	order_id = models.AutoField(primary_key=True)
+	user_id = models.IntegerField(foreign_key=True)
+	product_id = models.IntegerField(foreign_key=True)
+	quantity = models.IntegerField()
+	order_date = models.DateTimeField(auto_now_add=True)
