07d9b580ecf0040f3e9820a3e7ebb18da97b6707
erica <you@example.com>
2024-10-25 16:19:18 +0100

    Add create_user methon in views

--- srcs/market/users/views.py
+++ srcs/market/users/views.py
@@ -2,6 +2,14 @@ from django.shortcuts import render
 from .models import User
 
 
+def create_user(request):
+    if request.method == 'POST':
+        name = request.POST['name']
+        email = request.POST['email']
+        User.objects.create(name=name, email=email)
+        return redirect('list_users') 
+    return render(request, 'users/create_user.html')
+
 def list_users(request):
     users = User.objects.all()
     return render(request, 'users/list_users.html', {'users': users})
