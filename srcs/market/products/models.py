dd6876a65d8fe5bc6208a5cd0cdd1af88549b7c7
jhonata <jhon@DESKTOP-MAPFS0I.>
2024-10-25 14:37:38 +0100

    feat: user and product.models

--- srcs/market/products/models.py
+++ srcs/market/products/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+	product_id = models.AutoField(primary_key=True)
+	product_name = models.CharField(max_length=100)
+	price = models.DecimalField(max_digits=10, decimal_places=2)
